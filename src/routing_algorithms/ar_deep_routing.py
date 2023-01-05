- fix calcolo valori dqn (spostamento softmax)

src/routing_algorithms/ar_deep_routing.py
@@ -5,6 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 from src.routing_algorithms.BASE_routing import BASE_routing
 from src.utilities import utilities as util, config
@@ -16,20 +17,12 @@ if is_ipython:
 
 plt.ion()
 
-# BATCH_SIZE is the number of transitions sampled from the replay buffer
-# GAMMA is the discount factor as mentioned in the previous section
 # EPS_START is the starting value of epsilon
 # EPS_END is the final value of epsilon
 # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-# TAU is the update rate of the target network
-# LR is the learning rate of the AdamW optimizer
-BATCH_SIZE = 128
-GAMMA = 0.99
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 1000
-TAU = 0.005
-LR = 1e-4
 
 
 class ARDeepLearningRouting(BASE_routing):
@@ -46,7 +39,7 @@ class ARDeepLearningRouting(BASE_routing):
         # use this to have always the same simulation
         random.seed(self.simulator.seed)
 
-        self.omega = 0.2  # 0 < w < 1 used to adjust the importance of reliable distance Dij in reward function
+        self.omega = 0.3  # 0 < w < 1 used to adjust the importance of reliable distance Dij in reward function
 
         self.R_max = 1
 
@@ -75,9 +68,8 @@ class ARDeepLearningRouting(BASE_routing):
             # Exploit - choose the best action
             with torch.no_grad():
                 # get action from the Q-NN, giving current state
-                results = self.simulator.policy_net(state)
+                results = F.softmax(self.simulator.policy_net(state), dim=1)
                 return self.simulator.drones[torch.argmax(results).item()]
-
         else:
             # Explore - choose a random drone
             return self.simulator.rnd_routing.choice([v[1] for v in opt_neighbors])

src/routing_algorithms/deep_ql/dqn.py
@@ -95,8 +95,7 @@ class DQN(nn.Module):
         x = F.leaky_relu(self.layer1(x), negative_slope=0.4)
         x = F.leaky_relu(self.layer2(x), negative_slope=0.4)
         x = self.layer3(x)
-        result = F.softmax(torch.masked_fill(x, mask == 0, float('-inf')), dim=1)
-        return result
+        return torch.masked_fill(x, mask == 0, float('-inf'))
 
     def get_mask(self, state):
         complete_mask = []

src/simulation/metrics.py
@@ -3,6 +3,8 @@ import pickle
 
 import numpy as np
 
+from src.simulation import simulator
+
 """ Metrics class keeps track of all the metrics during all the simulation. """
 
 
@@ -10,6 +12,7 @@ class Metrics:
 
     def __init__(self, simulator):
 
+        self.mission_setup = {}
         self.simulator = simulator
 
         # The mean number of possible relays when i want to communicate
@@ -41,6 +44,10 @@ class Metrics:
 
         self.rewards_actions = {}
         self.mean_reward_actions = {}
+
+        self.train_count = 0
+        self.loss_trend = []
+        self.reward_trend = []
 
     def other_metrics(self):
         """
@@ -171,6 +178,12 @@ class Metrics:
             "time_on_active_routing": str(self.time_on_active_routing)
         }
 
+        if self.simulator.routing_algorithm.name == "ARDEEP_QL":
+            self.mission_setup.update({
+                "batch_size": simulator.BATCH_SIZE,
+                "learning_rate": simulator.LR,
+            })
+
     def __dictionary_represenation(self):
         """ compute the dictionary to save as json """
         self.other_metrics()
@@ -188,14 +201,17 @@ class Metrics:
         out_results["time_on_mission"] = self.time_on_mission
         out_results["packet_delivery_ratio"] = self.number_of_packets_to_depot / self.all_data_packets_in_simulation
         out_results["all_control_packets_in_simulation"] = self.all_control_packets_in_simulation
-        out_results["all_data_packets_in_simulation"] = self.all_data_packets_in_simulation
-        out_results["all_events"] = [ev.to_json() for ev in self.events]
+        # out_results["all_data_packets_in_simulation"] = self.all_data_packets_in_simulation
+        # out_results["all_events"] = [ev.to_json() for ev in self.events]
         out_results["not_listened_events"] = [ev.to_json() for ev in self.events_not_listened]
         out_results["events_delivery_times"] = [str(e) for e in self.event_delivery_times]
         out_results["drones_packets"] = [pck.to_json() for pck in self.drones_packets]
-        out_results["drones_to_depot_packets"] = [(pck.to_json(), delivery_ts) for pck, delivery_ts in
-                                                  self.drones_packets_to_depot]
+        # out_results["drones_to_depot_packets"] = [(pck.to_json(), delivery_ts) for pck, delivery_ts in self.drones_packets_to_depot]
         out_results["mean_number_of_relays"] = np.nanmean(self.mean_numbers_of_possible_relays)
+
+        out_results["train_count"] = self.train_count
+        out_results["loss_trend"] = self.loss_trend
+        out_results["reward_trend"] = self.reward_trend
 
         return out_results
 

src/simulation/simulator.py
@@ -21,9 +21,13 @@ as default all the parameters are set to be those in the config file. For extens
 you can initialize the Simulator with non default values. 
 """
 
+# LR is the learning rate of the AdamW optimize
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# TAU is the update rate of the target network
 LR = 1e-4
 BATCH_SIZE = 128
-GAMMA = 0.99
+GAMMA = 0.9
 TAU = 1  # 0.005
 
 
@@ -126,10 +130,8 @@ class Simulator:
             self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=LR, amsgrad=True)
             self.memory = ReplayMemory(10000)
 
-            self.weights_l1 = self.policy_net.layer1.weight.data.tolist()
-            self.weights_l2 = self.policy_net.layer2.weight.data.tolist()
-            self.weights_l3 = self.policy_net.layer3.weight.data.tolist()
-
+            self.loss_trend = []
+            self.reward_trend = []
             self.train_count = 0
 
         print("Device: ", config.DEVICE)
@@ -349,14 +351,7 @@ class Simulator:
         # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
-        # todo check if we can remove non_final_mask
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)),
-                                      device=config.DEVICE,
-                                      dtype=torch.bool)
-
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        next_state_batch = torch.cat(batch.next_state)
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
@@ -371,15 +366,17 @@ class Simulator:
         # on the "older" target_net; selecting their best reward with max(1)[0].
         # This is merged based on the mask, such that we'll have either the expected
         # state value or 0 in case the state was final.
-        next_state_values = torch.zeros(BATCH_SIZE, device=config.DEVICE)
         with torch.no_grad():
-            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
+            next_state_values = self.target_net(next_state_batch).max(1)[0]
+
         # Compute the expected Q values
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
         # Compute Huber loss
         criterion = nn.SmoothL1Loss()
         loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        self.loss_trend.append([self.cur_step, loss.item()])
 
         # Optimize the model
         self.optimizer.zero_grad()
@@ -408,6 +405,11 @@ class Simulator:
         print("Closing simulation")
         print("Len memory: ", len(self.memory))
         print("Train count: ", self.train_count)
+        print("Expired packet: ", len(self.expired_pkt))
+
+        self.metrics.train_count = self.train_count
+        self.metrics.loss_trend = self.loss_trend
+        self.metrics.reward_trend = self.reward_trend
 
         self.print_metrics(plot_id="final")
         # make sure to have output directory in the project